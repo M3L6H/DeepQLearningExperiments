642ee9981cc1400c2d9de521075406d442fb3f12
M3L6H <sisoac@hotmail.com>
2020-08-13 01:05:31 -0700

    Initial commit

---
+++ image_env.py
@@ -0,0 +1,93 @@
+from PIL import Image
+
+
+class Vector2D:
+  def __init__(self, x=0, y=0):
+    self.x, self.y = x, y
+    
+    if isinstance(x, list) or isinstance(x, tuple):
+      self.x = x[0]
+      self.y = y[0]
+    elif isinstance(x, Vector2D):
+      self.x = x.x
+      self.y = x.y
+
+
+  def __add__(self, other):
+    if isinstance(other, Vector2D):
+      return Vector2D(self.x + other.x, self.y + other.y)
+    elif isinstance(other, list) or isinstance(other, tuple):
+      return Vector2D(self.x + other[0], self.y + other[1])
+    elif isinstance(other, int) or isinstance(other, float):
+      return Vector2D(self.x + other, self.y + other)
+    else:
+      return NotImplemented
+
+
+  def __sub__(self, other):
+    if isinstance(other, Vector2D):
+      return Vector2D(self.x - other.x, self.y - other.y)
+    elif isinstance(other, list) or isinstance(other, tuple):
+      return Vector2D(self.x - other[0], self.y - other[1])
+    elif isinstance(other, int) or isinstance(other, float):
+      return Vector2D(self.x - other, self.y - other)
+    else:
+      return NotImplemented
+
+
+  # Performs dot multiplication in the case of another vector
+  def __mul__(self, other):
+    if isinstance(other, Vector2D):
+      return self.x * other.x + self.y * other.y
+    elif isinstance(other, list) or isinstance(other, tuple):
+      return self.x * other[0] + self.y * other[1]
+    elif isinstance(other, int) or isinstance(other, float):
+      return Vector2D(self.x * other, self.y * other)
+
+
+class Window:
+  def __init__(self, size, pos=Vector2D(0, 0), zoom=1):
+    self.size = size
+    self.pos = pos
+    self.zoom = zoom
+
+  
+  def __updateImage(self):
+    self.pix = Image.open(self.image_path).thumbnail(self.size * self.zoom).load()
+
+
+  def setImage(self, image_path):
+    self.image_path = image_path
+    self.__updateImage()
+
+  
+  def setZoom(self, zoom):
+    if not self.image_path:
+      raise RuntimeError("Cannot set zoom without image!")
+
+    self.zoom = zoom
+    self.__updateImage()
+
+
+  def translate(self, offset):
+    self.pos += offset
+
+
+class ImageEnv:
+  def __init__(self, window_size=64, num_windows=16):
+    self.window_size = window_size
+    self.num_windows = num_windows
+
+    print(f"Initialized image environment with\n\twindow_size: { window_size }\n\tnum_windows: { num_windows }")
+
+
+  def setImage(self, image_path):
+    self.image_path = image_path
+    self.image = Image.open(image_path)
+    self.pix = self.image.load()
+    print(f"Loaded { image_path }")
+
+
+env = ImageEnv()
+path = r"C:\Users\Michael\Desktop\Images\Ahsoka\Ahsoka-Hi-Res-0SFW-01.jpg"
+env.setImage(path)
